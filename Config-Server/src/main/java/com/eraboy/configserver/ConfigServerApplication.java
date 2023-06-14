a2529190df38d9fc0a0934fcdcef20a2d07091b6
oshadaera68 <mindartlk010@gmail.com>
2023-06-14 10:44:22 +0530

    updated content

---
+++ Config-Server/src/main/java/com/eraboy/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.eraboy.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class);
+    }
+}
